941a9d3e9c9e7b957c8c33ef0627cd45c33f5338
Remezov Dmitry <darknessdizi@gmail.com>
2022-09-07 00:35:30 +0800

    add class Mentor and add method "rate_hw"

--- class_students.py
+++ class_students.py
@@ -1,12 +1,54 @@
 class Student:
 
-    '''Создаем класс для студентов'''
+    '''Создает студентов'''
 
     def __init__(self, name, surname, gender):
 
         '''Конструктор класса Student.
-        Добавляет студента в класс и определяет его атрибуты'''
+        Добавляет студента и определяет его атрибуты'''
 
         self.name = name
         self.surname = surname
         self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def add_courses(self, course_name):
+        self.finished_course.append(course_name)
+ 
+ 
+class Mentor:
+
+    '''Создает учитилей'''
+
+    def __init__(self, name, surname):
+
+        '''Конструктор класса Mentor.
+        Добавляет учителя и определяет его атрибуты'''
+
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+ 
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+ 
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+ 
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+ 
+print(best_student.grades)
+
